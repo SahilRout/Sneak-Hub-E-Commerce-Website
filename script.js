a14a7deb0291db97a15a9e13e2051c1e711956fc
SahilRout <sahilrout07@gmail.com>
2021-09-12 20:44:44 +0530

    account-page

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-let menuItems = document.getElementById("menuItems");
+  /* Nav Bar */
 
-menuItems.style.maxHeight = "0px";
+  let menuItems = document.getElementById("menuItems");
 
-document.getElementById("dropDown").onclick = function() { menuToggle() };
+  menuItems.style.maxHeight = "0px";
 
-function menuToggle() {
-    if (menuItems.style.maxHeight == "0px") {
-        menuItems.style.maxHeight = "200px";
-    } else {
-        menuItems.style.maxHeight = "0px"
-    }
-}
+  document.getElementById("dropDown").onclick = function() { menuToggle() };
 
-let productImg = document.getElementById("product-img");
-let smallImg = document.getElementsByClassName("small-img");
+  function menuToggle() {
+      if (menuItems.style.maxHeight == "0px") {
+          menuItems.style.maxHeight = "200px";
+      } else {
+          menuItems.style.maxHeight = "0px"
+      }
+  }
 
-smallImg[0].onclick = function() {
-    productImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = function() {
-    productImg.src = smallImg[1].src;
-}
-smallImg[2].onclick = function() {
-    productImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = function() {
-    productImg.src = smallImg[3].src;
-}
+  /* Account Page */
+
+  let LoginForm = document.getElementById("loginForm");
+  let RegForm = document.getElementById("regForm");
+  let Indicator = document.getElementById("Indicator");
+
+  document.getElementById("Log-span").onclick = function() { login() };
+  document.getElementById("Reg-span").onclick = function() { register() };
+
+  function register() {
+      RegForm.style.transform = "translateX(0px)";
+      LoginForm.style.transform = "translateX(0px)";
+      Indicator.style.transform = "translateX(100px)";
+  }
+
+  function login() {
+      RegForm.style.transform = "translateX(300px)";
+      LoginForm.style.transform = "translateX(300px)";
+      Indicator.style.transform = "translateX(0px)";
+  }
+
+  /* Product page*/
+
+  let productImg = document.getElementById("product-img");
+  let smallImg = document.getElementsByClassName("small-img");
+
+  smallImg[0].onclick = function() {
+      productImg.src = smallImg[0].src;
+  }
+  smallImg[1].onclick = function() {
+      productImg.src = smallImg[1].src;
+  }
+  smallImg[2].onclick = function() {
+      productImg.src = smallImg[2].src;
+  }
+  smallImg[3].onclick = function() {
+      productImg.src = smallImg[3].src;
+  }
