f16ba650320382459e4ee99a0b32ed6b9cb99d39
SahilRout <sahilrout07@gmail.com>
2021-09-12 15:17:27 +0530

    product-details

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function menuToggle() {
         menuItems.style.maxHeight = "0px"
     }
 }
+
+let productImg = document.getElementById("product-img");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function() {
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    productImg.src = smallImg[3].src;
+}
