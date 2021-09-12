9dbe5331c18999d601f33b56f5634945ad65f6b7
SahilRout <sahilrout07@gmail.com>
2021-09-12 21:14:14 +0530

    js

--- script.js
+++ script.js
@@ -15,40 +15,41 @@
   }
 
   /* Account Page */
+  window.onload = function() {
+          let LoginForm = document.getElementById("loginForm");
+          let RegForm = document.getElementById("regForm");
+          let Indicator = document.getElementById("Indicator");
 
-  let LoginForm = document.getElementById("loginForm");
-  let RegForm = document.getElementById("regForm");
-  let Indicator = document.getElementById("Indicator");
+          document.getElementById("Log-span").onclick = function() { login() };
+          document.getElementById("Reg-span").onclick = function() { register() };
 
-  document.getElementById("Log-span").onclick = function() { login() };
-  document.getElementById("Reg-span").onclick = function() { register() };
+          function register() {
+              RegForm.style.transform = "translateX(0px)";
+              LoginForm.style.transform = "translateX(0px)";
+              Indicator.style.transform = "translateX(100px)";
+          }
 
-  function register() {
-      RegForm.style.transform = "translateX(0px)";
-      LoginForm.style.transform = "translateX(0px)";
-      Indicator.style.transform = "translateX(100px)";
-  }
+          function login() {
+              RegForm.style.transform = "translateX(300px)";
+              LoginForm.style.transform = "translateX(300px)";
+              Indicator.style.transform = "translateX(0px)";
+          }
+      }
+      /* Product page*/
+  window.onload = function() {
+      let productimage = document.getElementById("product-img");
+      let smallimage = document.getElementsByClassName("small-img");
 
-  function login() {
-      RegForm.style.transform = "translateX(300px)";
-      LoginForm.style.transform = "translateX(300px)";
-      Indicator.style.transform = "translateX(0px)";
-  }
-
-  /* Product page*/
-
-  let productImg = document.getElementById("product-img");
-  let smallImg = document.getElementsByClassName("small-img");
-
-  smallImg[0].onclick = function() {
-      productImg.src = smallImg[0].src;
-  }
-  smallImg[1].onclick = function() {
-      productImg.src = smallImg[1].src;
-  }
-  smallImg[2].onclick = function() {
-      productImg.src = smallImg[2].src;
-  }
-  smallImg[3].onclick = function() {
-      productImg.src = smallImg[3].src;
+      smallimage[0].onclick = function() {
+          productimage.src = smallimage[0].src;
+      }
+      smallimage[1].onclick = function() {
+          productimage.src = smallimage[1].src;
+      }
+      smallimage[2].onclick = function() {
+          productimage.src = smallimage[2].src;
+      }
+      smallimage[3].onclick = function() {
+          productimage.src = smallimage[3].src;
+      }
   }
